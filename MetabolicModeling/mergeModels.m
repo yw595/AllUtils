67ff202e6f65bdef6b5fd742b4a7e8c8b0f072da
Yiping Wang <yw595@cornell.edu>
2018-10-12 11:12:39 -0400

    synchronize

--- MetabolicModeling/mergeModels.m
+++ MetabolicModeling/mergeModels.m
@@ -19,10 +19,17 @@ for l=1:length(idxsToIter)
         returnModel.rxns{end+1} = modelTemp.rxns{j};
         returnModel.rxnNames{end+1} = modelTemp.rxnNames{j};
         returnModel.subSystems{end+1} = modelTemp.subSystems{j};
-        returnModel.rxnECNums{end+1} = '';
+        if isfield(returnModel,'rxnECNumbers')
+	    returnModel.rxnECNumbers{end+1} = modelTemp.rxnECNumbers{j};
+        end
+        if isfield(returnModel,'rxnECNums')
+            returnModel.rxnECNums{end+1} = '';
+        end
         returnModel.lb(strcmp(returnModel.rxns,modelTemp.rxns{j})) = modelTemp.lb(j);
         returnModel.ub(strcmp(returnModel.rxns,modelTemp.rxns{j})) = modelTemp.ub(j);
-        returnModel.rev(strcmp(returnModel.rxns,modelTemp.rxns{j})) = modelTemp.rev(j);
+        if isfield(returnModel,'rev')
+            returnModel.rev(strcmp(returnModel.rxns,modelTemp.rxns{j})) = modelTemp.rev(j);
+        end
         if isfield(returnModel,'c')
             returnModel.c(strcmp(returnModel.rxns,modelTemp.rxns{j})) = modelTemp.c(j);
         end
@@ -35,12 +42,16 @@ for l=1:length(idxsToIter)
         metNames = modelTemp.metNames(modelTemp.S(:,j)~=0);
         coeffs = modelTemp.S(modelTemp.S(:,j)~=0,j);
         bCoeffs = modelTemp.b(modelTemp.S(:,j)~=0);
+        csenseCoeffs = modelTemp.csense(modelTemp.S(:,j)~=0);
         for k=1:length(mets)
             if ~any(strcmp(returnModel.mets,mets{k}))
                 returnModel.mets{end+1} = mets{k};
                 returnModel.metNames{end+1} = metNames{k};
                 if isfield(returnModel,'b')
                     returnModel.b(end+1) = bCoeffs(k);
+                end
+		if isfield(returnModel,'csense')
+		    returnModel.csense(end+1) = csenseCoeffs(k);
                 end
             end
             returnModel.S(strcmp(returnModel.mets,mets{k}),strcmp(returnModel.rxns,modelTemp.rxns{j})) = coeffs(k);
